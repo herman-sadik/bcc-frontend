e03028737dadcb613f2c407d5b99e835e6ddc60c
Adrian <adrom99@gmail.com>
2020-02-19 13:41:52 +0100

    order keys

--- src/transactions/createAccount.js
+++ src/transactions/createAccount.js
@@ -12,15 +12,34 @@ const {invokeScript} = require('@waves/waves-transactions')
 
 
 const createAccount = async data => {
-  console.log(broadcast)
+
+  const keys = {
+    user_address: '3MEXjT3hfcNiweB1wF76oa8o3R9c7FboFF6',
+    user_public_key: '7jMtJvxupPhs8pijnjH3EvrfQ3V6KZsWF2uYFgnbCdCR',
+    user_private_key: '98kpLeeRa1g793JoS924BWYhDbDDVx2DYnV9ND4rnhLK',
+    dapp: '3M7BPTZZgqa6JcXqAg7ZiDB2wuChxAxPtgx',
+    asset_id: 'Gci8aULLGc97RMu3WAHombcERuTER3NV54YSrNjgdEW2'
+  }
+
+  const stuff = {
+    privateKey: '98kpLeeRa1g793JoS924BWYhDbDDVx2DYnV9ND4rnhLK'
+  }
   const iTx = invokeScript({
     dApp: "3M7BPTZZgqa6JcXqAg7ZiDB2wuChxAxPtgx",
     call: { function: "createAccount" },
-    payment: [{ assetId: null, amount: 10 }]
-  }, "favorite often like wheel doctor leisure arch share oxygen hobby fantasy sad require embrace man")
-  console.log(JSON.stringify(iTx))
-  const test = await broadcast(iTx)
+    payment: [{ assetId: 'Gci8aULLGc97RMu3WAHombcERuTER3NV54YSrNjgdEW2', amount: 10 }],
+    chainId: 82,
+  }, stuff)
 
+  const txObj = {
+    amount: 800000000,
+    recipient: '3MEXjT3hfcNiweB1wF76oa8o3R9c7FboFF6',
+    assetId: 'Gci8aULLGc97RMu3WAHombcERuTER3NV54YSrNjgdEW2'
+  }
+  console.log(JSON.stringify(transfer(txObj, '3M7BPTZZgqa6JcXqAg7ZiDB2wuChxAxPtgx')))
+
+  // let txTransfer = await broadcast(transfer(txObj, ))
+  const test = await broadcast(iTx, 'http://localhost:6869')
 }
 
 export default createAccount
