cc1eb1d0a5e8fcbff80639576851bf745269b500
Adrian <adrom99@gmail.com>
2020-02-20 12:36:15 +0100

    df

--- src/transactions/createAccount.js
+++ src/transactions/createAccount.js
@@ -4,7 +4,7 @@ const {invokeScript} = require('@waves/waves-transactions')
 const nodeUrl = 'http://localhost:6869'
 const multiplier = 10 ** 8
 const chainId = 82
-const dappAddress = '3MPgKkfBngNg3kog5a9Y3zMbAt8GsVfa2zK'
+const dappAddress = '3MFZUNgg6FZgBtkusHTwMLuqPy829uJxLGe'
 
 export const createAccount = async data => {
 
@@ -16,9 +16,9 @@ export const createAccount = async data => {
   const iTx = invokeScript({
     dApp: dappAddress,
     call: { function: "createAccount" },
-    payment: [{ assetId: "63dfCSFSuwM4NrP7pgTuuaNv2vRA6sWUrevxKXJkBE14", amount: 10 * multiplier }],
+    payment: [{ assetId: "BMmVyWisTfk4aLCVSm3iAzjjBLVAHtqQcUWREMXm3Zj7", amount: 10 * multiplier }],
     chainId: chainId,
-  }, {privateKey: 'BEjRFEyr7FEtjGqbUGnKvqEpM3SuixMHDVE98hNeNvyZ'})
+  }, {privateKey: '6wExKRKNJDnE3DH2iGJUZ63vkpVckQANtApTkoA8jFbi'})
 
   await broadcast(iTx, nodeUrl)
   await waitForTx(iTx.id)
