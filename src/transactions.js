ef0a5af0c92f9e50b0528b9c4ea63826b2abd5b7
Adrian <adrom99@gmail.com>
2020-02-20 14:57:28 +0100

    asd

--- src/transactions.js
+++ src/transactions.js
@@ -1,12 +1,10 @@
-import {broadcast, waitForTx, nodeInteraction} from '@waves/waves-transactions'
-const {invokeScript} = require('@waves/waves-transactions')
+import {nodeInteraction} from '@waves/waves-transactions'
 
 const nodeUrl = 'http://localhost:6869'
 const multiplier = 10 ** 8
 const chainId = 82
 const dappAddress = '3MNhntdoSd9btJxKzbFytt3CF85JuminoKe'
 const assetId = 'AMxocLehhNMrvY9AzMpeBuPNBknKBX4KtMW5D3YBJH6C'
-const privateKey = '6wExKRKNJDnE3DH2iGJUZ63vkpVckQANtApTkoA8jFbi'
 
 const getData = key => {
   return nodeInteraction.accountDataByKey(key, dappAddress, nodeUrl)
@@ -20,41 +18,35 @@ const invoke = tx => {
   }
 }
 
-export const createAccount = async () => {
-  const tx = invokeScript({
-    dApp: dappAddress,
-    call: {function: 'createAccount'},
-    payment: [{ assetId: assetId, amount: 10 * multiplier }],
-    chainId: chainId,
-  }, {privateKey: privateKey})
-
-  await broadcast(tx, nodeUrl)
-  await waitForTx(tx.id)
-  console.log(tx)
-}
-
 export const getUsers = async () => {
   const res = await getData('asset_id')
   console.log(res)
 }
 
-export const deposit = async amount => {
+export const createAccount = () => {
+  return invoke({
+    dApp: dappAddress,
+    call: {function: 'createAccount'},
+    payment: [{ assetId: assetId, amount: 10 * multiplier }],
+    chainId: chainId,
+  })
+}
+
+export const deposit = amount => {
   if (!amount) {
     console.error('Provide amount of tokens as an argument')
     return
   }
-  const tx = invokeScript({
+  return invoke({
     dApp: dappAddress,
     call: {function: 'deposit'},
     payment: [{ assetId: assetId, amount: amount * multiplier }],
     chainId: chainId
-  }, {privateKey: privateKey})
-  console.log(tx)
-  await broadcast(tx, nodeUrl)
+  })
 }
 
-export const createDevice = async () => {
-  const tx = invokeScript({
+export const createDevice = () => {
+  return invoke({
     dApp: dappAddress,
     call: {
       function: 'createDevice',
@@ -64,7 +56,5 @@ export const createDevice = async () => {
       ]
     },
     chainId: chainId
-  }, {privateKey: privateKey})
-  await broadcast(tx, nodeUrl)
-  console.log(tx)
+  })
 }
