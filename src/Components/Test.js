63671cdbaf0d376d1989a8f4bccdcd6d7777026e
Adrian <adrom99@gmail.com>
2020-02-21 09:41:03 +0100

    before0

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,19 +1,16 @@
 import React from 'react'
-import {deposit, createDevice, getUsers, createAccount} from '../transactions'
+import * as transactions from '../transactions'
 
 import {withWaves} from '../transactions/createAccount'
-
-
 import '../Styles/Test.css'
 
 const Test = () => {
 
   return ( 
     <div className='test'>
-      <button onClick={() => withWaves(createAccount)}>createAccount</button>
-      <button onClick={() => withWaves(deposit)}>deposit 25</button>
-      <button onClick={() => withWaves(createDevice)}>createDevice</button>
-      {/* <button onClick={() => withWaves(getUsers)}>getUsers</button> */}
+      <button onClick={() => withWaves(transactions.createAccount)}>createAccount</button>
+      <button onClick={() => withWaves(transactions.deposit)}>deposit 25</button>
+      <button onClick={() => withWaves(transactions.createDevice)}>createDevice</button>
     </div>
    )
 }
--- src/transactions.js
+++ src/transactions.js
@@ -3,16 +3,21 @@ import {nodeInteraction} from '@waves/waves-transactions'
 const nodeUrl = 'http://localhost:6869'
 const multiplier = 10 ** 8
 const chainId = 82
-const dappAddress = '3M7tbn774Vgb3vMGpqYnTgzdrxFfusJ9cvV'
-const assetId = '7tmWnCQZHGbmhKsQ7e9RhxiPkowwUtp4YCVXKvbbreEA'
+const dappAddress = '3M4sJHV71NzWpFWKpUU5ki62Pqu3PCPbxiZ' 
+const assetId = '44MoU2AW1J497TCSjXqRbAjWHARrbn2NgGccJ5eWLC65'
 
 const getData = key => {
   return nodeInteraction.accountDataByKey(key, dappAddress, nodeUrl)
 }
 
+export const getAssetId = async () => {
+  const res = await getData('asset_id')
+  if (res) return res.value
+  return null
+}
+
 const invoke = tx => {
   tx.type = 16
-  
   tx.data.fee = {
     tokens: '0.005',
     assetId: 'WAVES'
@@ -21,8 +26,7 @@ const invoke = tx => {
 }
 
 export const getUsers = async () => {
-  const res = await getData('asset_id')
-  console.log(res)
+  const res = await getData('asset_id').value
 }
 
 export const createAccount = () => {
